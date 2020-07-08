8c2ce9f70b8d246a2f012bec66bd7164045e50c1
Manoj Waikar <manoj.waikar@intercax.com>
2020-07-08 19:04:21 +0530

    Add fipp as a dependency.

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-				 [cc.qbits/alia-all "4.3.3"]
+                 [cc.qbits/alia-all "4.3.3"]
                  [cc.qbits/hayt "4.1.0"]
-                 [com.taoensso/timbre "4.10.0"]]
+                 [com.taoensso/timbre "4.10.0"]
+                 [fipp "0.6.23"]]
   :repl-options {:init-ns sqlforcql.core})
--- src/sqlforcql/core.clj
+++ src/sqlforcql/core.clj
@@ -2,9 +2,11 @@
   (:require [sqlforcql.db :as db]
             [sqlforcql.atoms :as atoms]))
 
-(defn connect-to-default-db []
-  (let [db-map (db/connect-to-db "localhost" "cassandra" "cassandra" "sqlforcql")]
-    (reset! atoms/default-db-map db-map)))
+(defn connect-to-default-db
+  ([] (connect-to-default-db "keyspace-name"))
+
+  ([keyspace] (let [db-map (db/connect-to-db "localhost" "cassandra" "cassandra" keyspace)]
+                (reset! atoms/default-db-map db-map))))
 
 (defn disconnect-from-default-db []
   (db/disconnect-from-db (deref atoms/default-db-map)))
